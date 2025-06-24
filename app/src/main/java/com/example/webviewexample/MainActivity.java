7716d8f70bd7c1721f4307abf7ecb7d011af4a0c
monir71 <zamanmonir71@yahoo.com>
2025-06-24 22:30:29 +0600

    Hi 5

---
+++ app/src/main/java/com/example/webviewexample/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.webviewexample;
+
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    WebView webView;
+    ProgressBar pBar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.webView);
+        pBar = findViewById(R.id.pBar);
+
+        webView.loadUrl("https://google.com/");
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient(){
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                pBar.setVisibility(View.VISIBLE);
+                super.onPageStarted(view, url, favicon);
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                pBar.setVisibility(View.GONE);
+                super.onPageFinished(view, url);
+            }
+        });
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+
+        if(webView.canGoBack())
+            webView.goBack();
+        else
+            super.onBackPressed();
+    }
+}
